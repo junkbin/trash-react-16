40ee9da8518990a466ebb044a374344abe4dd1c5
santosh <santosh.ece06@gmail.com>
2018-12-01 15:52:16 +0530

    update.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {"bg" : 'tomato'};
+
+    this.changeBackground = this.changeBackground.bind(this);
+  }
+
+  changeBackground() {
+    console.log("ChangeAAA");
+    this.setState({"bg" : 'royalblue'});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Make-center H-100 Bgfy" style={{'backgroundColor': this.state.bg}}>
+        <input className="btn" style={{background: this.state.bg}} type="button" value="CLICK ME" onClick={this.changeBackground} />
       </div>
     );
   }
